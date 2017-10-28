1d3ea39a5e83fb5bbbbf3679903f298f2e20f669
Siomara Nieves <snieves2014@my.fit.edu>
2017-10-28 15:42:35 -0700

    Update csv.py

--- src/csv.py
+++ src/csv.py
@@ -1,28 +1,10 @@
 import csv
 import numpy as np
-
-#things to do
-
-#either have the provider have the names for data as "DATE" and "DATA"
-#or have python know that if its a string then skip it, tried but then read all
-#numbers as strings
-
-#the test file being used has dates with no data, where they put a period
-
-#maybe if there's also blank space just skip it
-def array_return_values(info):
-    arrayinfo = np.asanyarray(info)
-    return arrayinfo
-
-def array_return_dates(dates):
-    arraydates = np.asanyarray(dates)
-    return arraydates
-
-def read_file():
-    np.set_printoptions(threshold=np.inf)
-    with open('test.csv') as csvfile: #for now just reading it from my computer
-        readCSV = csv.reader(csvfile, delimiter = ',') 
-        
+def read_file():    
+    print("Enter input file:")
+    with open(input()) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter = ',') #csv = comma separated
+                
         dates = []
         info = []
         
@@ -32,22 +14,15 @@ def read_file():
     
             if time == "." or data == ".":   #where there's a date with no data put ' . '
                 continue
-            
-            elif time == "DATE" and data == "DATA": #assuming first row has date and date strings, check above
-                continue
-                
+                            
             elif time == " " or data == " ": #testing for blanks
                 continue
                 
             else:                           #only add them in here
                 dates.append(time)
                 info.append(data)
-                
-        dates = list(map(float,dates))      #change all strings into floats 
-        info = list(map(float,info))
-       
- #test   arraydates = array_return_values(info)
- #test   print(arraydates)
+                            
+        return (info, dates)
 
 
         
