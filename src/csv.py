77ac89ebd7eb47a505317c5e46f87b0e17d78c9e
Siomara Nieves <snieves2014@my.fit.edu>
2017-10-28 12:39:29 -0700

    Return for arrays

--- src/csv.py
+++ src/csv.py
@@ -1,9 +1,5 @@
 import csv
-import scipy
 import numpy as np
-from plotly.offline import download_plotlyjs, init_notebook_mode,  plot
-from plotly.graph_objs import *
-init_notebook_mode()
 
 #things to do
 
@@ -14,64 +10,46 @@ init_notebook_mode()
 #the test file being used has dates with no data, where they put a period
 
 #maybe if there's also blank space just skip it
+def array_return_values(info):
+    arrayinfo = np.asanyarray(info)
+    return arrayinfo
 
+def array_return_dates(dates):
+    arraydates = np.asanyarray(dates)
+    return arraydates
 
-np.set_printoptions(threshold=np.inf)
-with open('test.csv') as csvfile: #for now just reading it from my computer
-    readCSV = csv.reader(csvfile, delimiter = ',') 
-    
-    dates = []
-    info = []
-    
-    for row in readCSV:
-        time = row[0]
-        data = row[1]
-
-        if time == "." or data == ".":   #where there's a date with no data put ' . '
-            continue
+def read_file():
+    np.set_printoptions(threshold=np.inf)
+    with open('test.csv') as csvfile: #for now just reading it from my computer
+        readCSV = csv.reader(csvfile, delimiter = ',') 
         
-        elif time == "DATE" and data == "DATA": #assuming first row has date and date strings, check above
-            continue
+        dates = []
+        info = []
+        
+        for row in readCSV:
+            time = row[0]
+            data = row[1]
+    
+            if time == "." or data == ".":   #where there's a date with no data put ' . '
+                continue
             
-        elif time == " " or data == " ": #testing for blanks
-            continue
-            
-        else:                           #only add them in here
-            dates.append(time)
-            info.append(data)
-            
-    dates = list(map(float,dates))      #change all strings into floats 
-    info = list(map(float,info))
-    
-    arraydates = np.asarray(dates)   #change list into array
-    arrayinfo = np.asanyarray(info)  
-    
-    z_score = []
-    
-    if len(dates) == len(info):       #do Q1
-        zscore = (scipy.stats.zscore(arrayinfo)) #calculate zscore
-        
-        trace = go.Scatter(  #ignore this just playing around 
-                x = dates,
-                y = zscore,
-                line = dict(
-                        color = ('rgb(205,12,24)'),
-                        width = 4
-                        )
-        )
-        
-        data = [trace]
-        layout = Layout(
-                showlegend=False,
-                height=600,
-                width=600,
-                )
-        fig = dict( data=data, layout=layout )
-        plot(fig)  #a graph, just testing
-        
+            elif time == "DATE" and data == "DATA": #assuming first row has date and date strings, check above
+                continue
+                
+            elif time == " " or data == " ": #testing for blanks
+                continue
+                
+            else:                           #only add them in here
+                dates.append(time)
+                info.append(data)
+                
+        dates = list(map(float,dates))      #change all strings into floats 
+        info = list(map(float,info))
+       
+ #test   arraydates = array_return_values(info)
+ #test   print(arraydates)
 
-    else:
-        print("columns are not of same length")
+
         
         
         
