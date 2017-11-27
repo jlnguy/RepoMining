b6ba3b0f578b84f551414f33a843d6dfad197ce6
Jessica Nguy <jnguy2014@my.fit.edu>
2017-11-26 20:43:20 -0500

    Delete UserInput.py

--- src/UserInput.py
+++
@@ -1,5 +0,0 @@
-# Python code for UserInput
-# Currently prompts user for input and displays input back.
-
-targetVariable = (input("Select Target Variable: "))
-print("The Target Variable is:", targetVariable)
