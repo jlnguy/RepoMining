b5dc36449685fe88a9c13481853de44345b4157d
Jessica Nguy <jnguy2014@my.fit.edu>
2018-02-11 18:51:13 -0500

    Edit for M4(5??)

--- src/ParseData.py
+++ src/ParseData.py
@@ -4,55 +4,45 @@
 from datetime import datetime
 import numpy as np
 
-#def shuffleR(filename, lag):
-def shuffle(dataX, start, lag):
+def shuffleDate(dataX, start, lag):
     # returns x-Axis data, y-Axis data, lag#, and file name, x-axis, y-axis
-    #dataX, dataY, xTitle, yTitle = getData(filename)
-    #start = getStart()
-    #print(start)
     start = datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
-    #print("into Loop:")
     count = 0
     for item in dataX:
-        #print('')
         if (item == start):
-            #print('match!')
-            #print(item, start)
             break
         elif (item != start):
-            #print('no match!')
-            #print(item, start)
             count = count+1
 
-    #print(count)
     # AND NOW WE ROLL
-    '''
-    print('')
-    print('count: ', count)
-    print("index: ", index)
-    '''
+
     index = count - lag
     #dataX = np.roll(dataX, index)
-    '''
-    print('Initial Data:')
-    for item in dataX:
-       print(item)
-    print('')
-    '''
+
     # Delete off rolled stuff
-    #print('Deleting off Rolled')
     n = 0
     while (n < index):
         dataX = np.delete(dataX, 0)
         n = n+1
 
-    '''
-    for item in dataX:
-        print(item)
-    '''
     return dataX
 
-# Find out where in the data target[0] timestamp = second data[x]
+def shuffleY(targetY, variableY, lag):
+    length = len(variableY) - len(targetY)
+    length = length - lag
+    ctr = 0
+    while (ctr < length):
+        variableY = np.delete(variableY, 0)
+        ctr = ctr+1
 
-#oname = 't3.csv'
-#shuffle(oname, '2015-12-3 12:12:12', 2)
+    return variableY
+
+def trimData(targetY, variableY):
+    # trim according to how long targetY is.
+    # idgaf bout variableY cuase it should be longer from going through NarrowData
+    lenTar = len(targetY)
+    ctr = len(variableY) - lenTar
+    while(ctr > 0):
+        variableY = np.delete(variableY, len(variableY) - 1)
+        ctr = len(variableY) - lenTar
+    return variableY
